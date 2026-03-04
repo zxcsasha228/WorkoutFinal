3b219875a3181ada694f5ebcfeea09ab783892d9
zxcsasha228 <sasha-gromov-2010@mail.ru>
2026-03-04 06:31:10 +0300

    Настроил статику для клиента

--- WorkoutSimple/WorkoutApi/WorkoutApi/Program.cs
+++ WorkoutSimple/WorkoutApi/WorkoutApi/Program.cs
@@ -1,13 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 using WorkoutApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.UseUrls("http://0.0.0.0:5000");
 
-// Временно закомментируем базу данных
-// builder.Services.AddDbContext<AppDbContext>(options =>
-//     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 builder.Services.AddCors();
@@ -15,13 +15,30 @@ builder.Services.AddCors();
 var app = builder.Build();
 
 app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-app.MapControllers();
 
-// Временно закомментируем создание БД
-// using (var scope = app.Services.CreateScope())
-// {
-//     var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-//     context.Database.EnsureCreated();
-// }
+// Настройка статических файлов для клиента
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// Обслуживание статических файлов из папки клиента
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "../WorkoutApp/wwwroot")),
+    RequestPath = ""
+});
+
+app.MapControllers();
+app.MapFallbackToFile("index.html", new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "../WorkoutApp/wwwroot"))
+});
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+}
 
 app.Run();
