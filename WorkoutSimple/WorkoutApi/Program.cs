fbd9cbeec41928c7c23c73115a55560d006f426e
zxcsasha228 <sasha-gromov-2010@mail.ru>
2026-03-04 06:11:55 +0300

    Временно отключил БД для теста

--- WorkoutSimple/WorkoutApi/Program.cs
+++ WorkoutSimple/WorkoutApi/Program.cs
@@ -3,11 +3,11 @@ using WorkoutApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Явно указываем порт 5000
 builder.WebHost.UseUrls("http://0.0.0.0:5000");
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Временно закомментируем базу данных
+// builder.Services.AddDbContext<AppDbContext>(options =>
+//     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 builder.Services.AddCors();
@@ -17,11 +17,11 @@ var app = builder.Build();
 app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 app.MapControllers();
 
-// Создаем базу данных если её нет
-using (var scope = app.Services.CreateScope())
-{
-    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    context.Database.EnsureCreated();
-}
+// Временно закомментируем создание БД
+// using (var scope = app.Services.CreateScope())
+// {
+//     var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+//     context.Database.EnsureCreated();
+// }
 
 app.Run();
